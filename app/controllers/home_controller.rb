f42d6621a331a5a816950f7e96ed5dbaa40d8130
Matthew Ager <matthew.a.ager@googlemail.com>
2013-08-30 18:47:14 +0200

    Added modules and fixed responsive design

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,14 @@
 class HomeController < ApplicationController
+
   def index
+
     @users = User.all
+
+    @user_profile = current_user.user_profile if current_user.present?
+
+    @news_posts = NewsPost.order('created_at DESC').limit(3)
+    @news_post_count = @news_posts.count
+
     @json = UserProfile.all.to_gmaps4rails do |user_profile, marker|
       marker.picture({
                          :picture => user_profile.logo_url(:marker).to_s,
@@ -18,5 +26,7 @@ class HomeController < ApplicationController
       )
       marker.infowindow render_to_string :partial => '/user_profiles/marker', :locals => { :object => user_profile }
     end
+
   end
+
 end
